3e9b6e93e4f6d3ddcec7cc3c45d50a909867a98e
Juan <148053476+jjuanx@users.noreply.github.com>
2025-05-10 18:19:10 +0200

    Delete Gitminer directory

--- Gitminer/src/main/java/aiss/gitminer/repositories/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package aiss.gitminer.repositories;
-
-import aiss.gitminer.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, String> {
-}
