21448330eade617bcda9cbd209d7a255b497e649
Juan <148053476+jjuanx@users.noreply.github.com>
2025-06-22 18:28:53 +0200

    Delete Gitminer directory

--- Gitminer/src/main/java/aiss/gitminer/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package aiss.gitminer.repositories;
-
-import aiss.gitminer.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
