9c3e64020112ee1948a634a65579542acfe97178
Juan <148053476+jjuanx@users.noreply.github.com>
2025-06-22 20:03:50 +0200

    Delete Gitminer directory

--- Gitminer/src/main/java/aiss/gitminer/repositories/CommitRepository.java
+++
@@ -1,9 +0,0 @@
-package aiss.gitminer.repositories;
-
-import aiss.gitminer.model.Commit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommitRepository  extends JpaRepository<Commit, String> {
-}
